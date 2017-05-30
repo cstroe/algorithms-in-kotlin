d9af650fee5d30693eabf96e9a060212c0e1ae6e
Cosmin Stroe <cosmin.stroe@uptake.com>
2017-05-30 08:41:43 -0500

    a working version of insertion sort

--- src/main/kotlin/com/github/cstroe/aik/sort/InsertionSort.kt
+++ src/main/kotlin/com/github/cstroe/aik/sort/InsertionSort.kt
@@ -1,5 +1,14 @@
 package com.github.cstroe.aik.sort
 
-fun insertionSort(ints: Array<Int>): Array<Int> {
-    return ints
+fun insertionSort(A: Array<Int>): Array<Int> {
+    for(j in 1 .. A.size - 1) {
+        val key = A[j]
+        var i = j - 1
+        while (i > -1 && A[i] > key) {
+            A.set(i + 1, A[i])
+            i = i - 1
+        }
+        A.set(i + 1, key)
+    }
+    return A
 }
--- src/test/kotlin/com/github/cstroe/aik/sort/InsertionSortTest.kt
+++ src/test/kotlin/com/github/cstroe/aik/sort/InsertionSortTest.kt
@@ -1,11 +1,32 @@
 package com.github.cstroe.aik.sort
 
 import org.junit.Test
+import java.util.*
 
 class InsertionSortTest {
+    val random = Random()
+
     @Test fun simpleSort() {
         val expected = arrayOf(1, 2, 4, 5, 7, 9)
         val output = insertionSort(arrayOf(1, 7, 5, 4, 9, 2))
         assert(expected contentEquals output, { -> "Output array ${output.contentDeepToString()} is not equal to ${expected.contentDeepToString()}" })
     }
+
+    @Test fun randomlySorted() {
+        val randomNumbers = (1 .. 10).map { random.nextInt() }.toTypedArray()
+        if(isSorted(randomNumbers)) {
+            val temp = randomNumbers[0]
+            randomNumbers[0] = randomNumbers[9]
+            randomNumbers[9] = temp
+        }
+
+        assert(!isSorted(randomNumbers))
+        val sortedArray = insertionSort(randomNumbers)
+        assert(isSorted(sortedArray), { -> "Array ${sortedArray.contentDeepToString()} is not sorted."})
+        assert(sortedArray.size == randomNumbers.size)
+        for(i in randomNumbers) {
+            assert(sortedArray.contains(i))
+        }
+    }
+
 }
